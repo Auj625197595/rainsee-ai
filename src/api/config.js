a2211dc5baa2559d065e712ead3e5ad620205d03
几乎拒绝艾米 <625197595@qq.com>
2026-03-15 15:00:45 +0800

    添加内置域名鉴权信息

--- src/api/config.js
+++ src/api/config.js
@@ -5,5 +5,5 @@
 export const API_BASE_DOMAIN = 'icon144.yjllq.com';
 export const API_URL = `https://${API_BASE_DOMAIN}/AiAssistantv2.php`;
 export const CHARACTER_STORE_URL = `https://${API_BASE_DOMAIN}/CharacterStore.php`;
-export const MARKITDOWN_API_URL = `https://${API_BASE_DOMAIN}/AiAssistantv2.php?action=markitdown`;
-export const WEBSCRAPE_API_URL = `https://${API_BASE_DOMAIN}/AiAssistantv2.php?action=webscrape`;
+export const MARKITDOWN_API_URL = `https://${API_BASE_DOMAIN}/AiAssistantv2.php?action=markitdown&key=sfjakjfhksdha`;
+export const WEBSCRAPE_API_URL = `https://${API_BASE_DOMAIN}/AiAssistantv2.php?action=webscrape&key=sfjakjfhksdha`;
